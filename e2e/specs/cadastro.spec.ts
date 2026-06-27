f6269d383c22e7c8f8a4bfd82eaffae2968f638c
begalve-lopes <begalve.927390438.00@gmail.com>
2026-06-27 17:13:11 +0100

    testde de cadastro conluido cenario feliz e infeliz

--- e2e/specs/cadastro.spec.ts
+++ e2e/specs/cadastro.spec.ts
@@ -1,11 +1,29 @@
 import { test } from "../setup/fixtures"
-import { gerarPerfil } from "../util/gerarPerfil"
+import { gerarPerfil, Perfil } from "../util/gerarPerfil"
 
-test.describe('Cadastro',()=>{
-    test('Deve conseguir fazer cadastro', async ({paginaCadastro})=>{
-        const novoUsuario =  gerarPerfil()
+test.describe('Cadastro', () => {
+    let novoUsuario: Perfil
+    test.beforeEach(async ({ paginaCadastro }) => {
+        await paginaCadastro.visitar();
+        novoUsuario = gerarPerfil();
+    })
+
+    test('Deve conseguir fazer cadastro', async ({ paginaCadastro }) => {
+
         await paginaCadastro.preencherFormulario(novoUsuario);
         await paginaCadastro.submeterFormulario();
         await paginaCadastro.cadastroFeitoComSucesso()
     })
+
+    test('Não Deve permitir usuário duplicado', async ({ paginaCadastro }) => {
+        await paginaCadastro.preencherFormulario(novoUsuario);
+        await paginaCadastro.submeterFormulario();
+        await paginaCadastro.cadastroFeitoComSucesso()
+
+        await paginaCadastro.visitar()
+        await paginaCadastro.preencherFormulario(novoUsuario);
+        await paginaCadastro.submeterFormulario();
+        await paginaCadastro.estaMostrandoMensagemErro('E-mail já utilizado.')
+    })
+
 })
