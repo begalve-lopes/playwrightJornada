d93100b961543aca3196036ceb5560b677ef64c2
begalve-lopes <begalve.927390438.00@gmail.com>
2026-06-27 23:23:56 +0100

    criar os worker moderno

---
+++ e2e/setup/testeLogado.ts
@@ -0,0 +1,51 @@
+import * as path from "path";
+import { test } from "./fixtures";
+import PaginaCadastro from "../page_objects/PaginaCadastro";
+import PaginaLogin from "../page_objects/PaginaLogin";
+import { gerarPerfil } from "../util/gerarPerfil";
+import { expect } from "@playwright/test";
+
+export const testeLogado = test.extend<object, { arquivoInfoLogado: string }>({
+    arquivoInfoLogado: [
+        async ({ browser }, use) => {
+            const id = test.info().parallelIndex;
+            const caminhoArquivo = path.resolve(
+                test.info().project.outputDir,
+                `.auth/usuario-${id}.json`
+            );
+
+            // 👇 Lê a baseURL diretamente do config do projeto
+            const baseURL = test.info().project.use.baseURL ?? 'http://localhost:4200';
+
+            const context = await browser.newContext({ baseURL });
+            const page = await context.newPage();
+
+            const paginaCadastro = new PaginaCadastro(page);
+            const paginaLogin = new PaginaLogin(page);
+            const novoUsuario = gerarPerfil();
+
+            // CADASTRO
+            await paginaCadastro.visitar();
+            await paginaCadastro.preencherFormulario(novoUsuario);
+            await paginaCadastro.submeterFormulario();
+
+            await expect(page).toHaveURL('/auth/login');
+
+            // LOGIN
+            await paginaLogin.fazerLogin(novoUsuario.email, novoUsuario.senha);
+            await paginaLogin.clicarBotaoAcessarConta()
+            await expect(page).toHaveURL('http://localhost:4200/home');
+
+
+            // SALVAR STATE
+            await context.storageState({ path: caminhoArquivo });
+
+            await use(caminhoArquivo);
+        },
+        { scope: "worker" }
+    ],
+
+    storageState: async ({ arquivoInfoLogado }, use) => {
+        await use(arquivoInfoLogado);
+    },
+});
---
+++ e2e/specs/edicaoDePerfil.spec.ts
@@ -0,0 +1,11 @@
+import { testeLogado } from "../setup/testeLogado";
+
+testeLogado.describe('Edição do Usuário',()=>{
+    testeLogado('Editar  perfil 1', async()=>{
+     
+    })
+
+    testeLogado('Editar  perfil 2', async()=>{
+     
+    })
+})
